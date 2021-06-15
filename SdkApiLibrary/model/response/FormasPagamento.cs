1dde79a5949b681709bb1db183474df1c5adf69a
Leandrini Neto <aoet@gft.com>
2021-06-15 15:01:09 -0300

    DTOs

---
+++ SdkApiLibrary/model/response/FormasPagamento.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SdkApiB2b.model.Response
+{
+    class FormasPagamento
+    {
+        [JsonProperty("idFormaPagamento")]
+        public int IdFormaPagamento { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+    }
+}
