82e59b07662d5123b66f43b8b572655350282345
Pinheiro da Rocha <msph@gft.com>
2021-06-16 11:12:45 -0300

    novo repositorio organizado

---
+++ sdk-api-b2b/SdkApiB2bTest/PedidoApiTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SdkApiB2bTest
+{
+    [TestClass]
+    public class PedidoApiTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("TestMethod1");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("TestMethod2");
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Console.WriteLine("TestMethod3");
+        }
+    }
+}
